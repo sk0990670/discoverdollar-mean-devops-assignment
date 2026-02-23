fb0a0c28fd2e8eac939a4b18bb3a7de825c95e7a
Sujeet Kumar <sk0990670@gmail.com>
2026-02-24 03:45:21 +0530

    Fix the db.config file

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: process.env.MONGODB_URI
+  // Use 'mongo' instead of 'localhost' when running in Docker
+  url: process.env.MONGODB_URI || "mongodb://mongo:27017/discoverdollar_db"
 };
