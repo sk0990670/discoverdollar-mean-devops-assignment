60a49f68d48cef27866490105440d712caa9a41b
Sujeet Kumar <sk0990670@gmail.com>
2026-02-24 02:58:12 +0530

    Fix the database issue

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    // apiUrl: 'http://localhost:8081/api'
-    apiUrl: '/api' // This is the API URL for the production environment
+    apiUrl: 'http://localhost:8081/api'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: true,
-    // apiUrl: 'http://localhost:8081/api'
-    apiUrl: '/api' // This is the API URL for the production environment
+    production: false,
+    apiUrl: 'http://localhost:8081/api'
 };
